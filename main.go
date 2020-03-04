c089caab9ee0846d1d6ebea8293c41099edb82d6
Christopher Johnson <36961199+critterjohnson@users.noreply.github.com>
2020-03-03 20:57:23 -0500

    fixing unmarshalling

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/critterjohnson/go-ask/alexarequest"
 	"github.com/critterjohnson/go-ask/alexaresponse"
 	"github.com/critterjohnson/go-ask/handlers"
@@ -23,5 +25,14 @@ func LambdaHandler(ctx context.Context, request alexarequest.Request) (alexaresp
 }
 
 func main() {
-	lambda.Start(LambdaHandler)
+	data, err := ioutil.ReadFile("sonnet138.json")
+	if err != nil {
+		fmt.Println("Error reading file", err)
+		return
+	}
+	request := alexarequest.Request{}
+	if err = json.Unmarshal(data, &request); err != nil {
+		fmt.Println("Error unmarshalling JSON", err)
+	}
+	fmt.Printf("%+v", request.Session)
 }
